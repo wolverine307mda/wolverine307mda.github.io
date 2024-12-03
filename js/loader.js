23a6fa65fbec165947a6820344c6e2e90047ade0
Mario de Domingo Alvarez <wolverine.mda.307@gmail.com>
2024-12-04 00:47:30 +0100

    adjust loader

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,7 @@
-window.addEventListener("load", function() {
-    // Oculta la pantalla de carga
-    document.getElementById("loader").style.display = "none";
-    // Muestra el contenido principal
-    document.querySelector(".pagina").style.display = "block";
+document.addEventListener("DOMContentLoaded", function() {
+    // Delay to ensure all resources are processed
+    setTimeout(function() {
+        document.getElementById("loader").style.display = "none";
+        document.querySelector(".pagina").style.display = "block";
+    }, 1500); // Optional delay to show loader (adjust as needed)
 });
